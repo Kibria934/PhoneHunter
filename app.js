94159441aaeabcafa806cb868235ae40dd0f1286
Kibria hossain <you@example.com>
2022-03-01 13:16:58 +0600

    others feature added

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ const error = (errorInput) => {
 const loadData = () => {
     const container = document.getElementById('second-container');
     container.innerHTML = ``;
-    const value = document.getElementById('search-field').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${value}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showPhone(data));
+    const detailsContainer = document.getElementById('details-display');
+    detailsContainer.innerHTML = ``
+    const value = document.getElementById('search-field').value.toLowerCase();
+    if (value == 'oppo' || value == 'apple' || value == 'iphone' || value == 'samsung' || value == 'huawei') {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${value}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => showPhone(data));
+    } else {
+        error('block')
+    }
 }
 
 const showPhone = name => {
-    const names = name.data.slice(0, 20)
+    const names = name.data.slice(1, 21)
     const value = document.getElementById('search-field').value;
     const container = document.getElementById('second-container');
     // error handaling
@@ -50,6 +56,9 @@ const showPhone = name => {
 
 // === === === === Explore button handler === === === === === === ===//
 const details = (id) => {
+    const detailsContainer = document.getElementById('details-display');
+    detailsContainer.innerHTML = ``
+
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
         .then(res => res.json())
@@ -59,26 +68,28 @@ const details = (id) => {
 
 const displayDetails = (input) => {
     console.log(input);
-    const detailsContainer = document.getElementById('details-display');
-    const div = document.createElement('div');
 
-    div.innerHTML = `
+    const { chipSet, sensor, releaseDate, others } = input;
+    console.log(releaseDate, sensor, others);
+    if (releaseDate != undefined || others != undefined) {
+        const detailsContainer = document.getElementById('details-display');
+        const div = document.createElement('div');
+
+        div.innerHTML = `
     <div class = " py-3 text-center">
         <img width="300" height="400" src="${input.image}" alt="">
         <h2>Name: <span class="text-success fw-semibold py-3">${input.name}</span> </h2>
         <h3>Brand Name: <span class="text-black fw-semibold py-3">${input.brand}</span> </h3>
         </div>        
-        <h3 id="mainFeature">Main Feature:  
+        <h3 id="mainFeature">Main Feature:  <p class="text-black fs-4 fw-semibold" id="chipset">Chipset: ${input.mainFeatures.chipSet}</p>
          <p class="text-black fs-4 fw-semibold">Display size: <span class="fs-5">${input.mainFeatures.displaySize}</span></p>
          <p class="text-black fs-4 fw-semibold">Memory: <span class="fs-5">${input.mainFeatures.memory}</span></p>
          <p class="text-black fs-4 fw-semibold">Storage: <span class="fs-5">${input.mainFeatures.storage}</span></p>
          </h3>
+         <h3>Other: <p class="text-black fs-4 fw-semibold">Butothe: <span class="fs-5">${input.others.Bluetooth}</span></p>
+         </h3>
                 `;
-    detailsContainer.appendChild(div)
-    const { chipSet, sensor, releaseDate, others } = input.mainFeatures;
-    // console.log(chipSet, releaseDate, sensor, others);
-    if (chipSet != undefined) {
-        div.innerHTML + `<p>Chipset: ${chipSet}</p>`;
+        detailsContainer.appendChild(div)
     } else {
         console.log('nai');
     }
